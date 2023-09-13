3bc5a2af40c15177f193c4ac0fd4f4df322b471e
noah <nattapong.w1004@gmail.com>
2023-09-14 04:41:16 +0700

    yolo-smartbin, add face recognition in facelogin function

--- object-detection/yolo-smartbin.py
+++ object-detection/yolo-smartbin.py
@@ -8,8 +8,8 @@ import json
 import redis
 import base64
 import paho.mqtt.client as mqtt
-
-
+import os
+import face_recognition
 
 # Global Variables for Threads
 worker_isrun = False
@@ -449,7 +449,64 @@ def task_facelogin(tag_login, tag_userdetail, tag_redeem, cam_source, face_sourc
 
 
 def facelogin(cam_source, face_source):
-    return "nat"
+    print("run face login, scanning face")
+    dir_list = os.listdir(face_source)
+    face_ref_encodings = []
+    face_names = []
+    # encode all face in face's folder
+    for file in dir_list:
+        face_name = os.path.splitext(file)[0]
+        face_ref = cv2.imread(face_source + file, cv2.IMREAD_COLOR)
+        face_ref_encoding = face_recognition.face_encodings(face_ref)[0]
+        face_ref_encodings.append(face_ref_encoding)
+        face_names.append(face_name)
+    print(face_names)
+
+    # looping for face recognition
+    stream = cv2.VideoCapture(cam_source)
+    print("start capture")
+    matched_name=None
+    skip_frame=0
+    compared_frame=0
+    while stream.isOpened():
+        ret, frame = stream.read()
+        if not frame is None:
+            skip_frame += 1
+            if skip_frame>=15:
+                # face recognition in here!
+                matched_name, frame = face_compare(frame, face_ref_encodings, face_names)
+                skip_frame=0
+                compared_frame += 1
+                if matched_name != None or compared_frame > 30:
+                    break
+            cv2.imshow("output frame", frame)
+            cv2.waitKey(1)
+    stream.release()
+
+    return matched_name
+
+def face_compare(face_input, face_ref_encodings, face_names):
+    face_locations = face_recognition.face_locations(face_input)
+    if face_locations == None or len(face_locations) != 1 :
+        return False, face_input # No face is detected or More than 1 face
+
+    matched_name = None
+    face_input_encodings = face_recognition.face_encodings(face_input, face_locations)
+    for face_input_encoding in face_input_encodings:
+        face_distances = face_recognition.face_distance(face_ref_encodings, face_input_encoding)
+
+        best_match_index = np.argmin(face_distances)
+        if face_distances[best_match_index] < 0.5:
+            matched_name = face_names[best_match_index]
+            # Draw a box around the face
+            top, right, bottom, left = face_locations[0]
+            cv2.rectangle(face_input, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            cv2.rectangle(face_input, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(face_input, face_names[best_match_index], (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    return matched_name, face_input
 
 def calcReward(objclass):
     pts=0
