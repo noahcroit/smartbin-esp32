e20a8c6575c18cacbc25c312ba925d1bf24f1d03
noah <nattapong.w1004@gmail.com>
2023-09-22 18:10:31 +0700

    yolo worker, add email feature

--- object-detection/yolo-smartbin.py
+++ object-detection/yolo-smartbin.py
@@ -602,7 +602,52 @@ def save_json_userdetail(user_dict, user_json):
         f.close()
 
 def send_email(userdetail):
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
     print("send email to ", userdetail["email"])
+
+    f = open("email-config.json")
+    data = json.load(f)
+    sender_email = data['sender_email']
+    recipient_email = userdetail["email"]
+    smtp_username = sender_email
+    smtp_password = data['sender_pwd']
+    f.close()
+
+    # Email configuration
+    subject = "Get free coffee from NIDA Hub Smartbin"
+    message = "This is your coupon from smartbin. You got 1 free cup of coffee"
+
+    # SMTP server configuration for Outlook
+    smtp_server = "smtp.office365.com"
+    smtp_port = 587  # For TLS
+
+    # Create the email message
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Establish a secure SMTP connection
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+
+        # Send the email
+        server.sendmail(sender_email, recipient_email, msg.as_string())
+
+        print("Email sent successfully!")
+    except Exception as e:
+        print("Error: Unable to send email.")
+        print(e)
+    finally:
+        # Close the SMTP server connection
+        server.quit()
+
 
 
 if __name__ == "__main__":
