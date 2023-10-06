de2d3700d319261e209f79a932df441ae0f559fe
SSA smartbin NIDA <SSA smartbin NIDA>
2023-10-06 03:07:29 -0400

    add startup script

--- smartbin-startup.sh
+++ smartbin-startup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
-cd ~/redis-server
-node server.js
-cd ~/kioskface
-npm start
+echo "setup ngrok for ssh"
+/snap/bin/ngrok tcp 22 &
+echo "setup redis-server"
+cd /home/smartbin/redis-server
+node server.js &
+echo "setup kiosk"
+cd /home/smartbin/kioskface
+npm start &
+sleep 5
+echo "setup smartbin worker"
+cd /home/smartbin/smartbin-esp32/object-detection
+/bin/python3 yolo-smartbin.py -s cam -c config.json -d false
