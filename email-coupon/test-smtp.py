b562e68b14d007bc71896210959b38e429f55421
noah <nattapong.w1004@gmail.com>
2024-12-25 00:05:11 +0700

    update example of image attachment on email

--- email-coupon/test-smtp.py
+++ email-coupon/test-smtp.py
@@ -1,45 +1,67 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import json
+import os
 
-# URL for video or camera source
-f = open("email-config.json")
-data = json.load(f)
-sender_email = data['sender_email']
-recipient_email = data['receiver_email']
-smtp_username = sender_email
-smtp_password = data['sender_pwd']
-f.close()
 
-# Email configuration
-subject = "Hello from Python!"
-message = "This is your coupon from smartbin. You got 1 free cup of coffee"
 
-# SMTP server configuration for Outlook
-smtp_server = "smtp.office365.com"
-smtp_port = 587  # For TLS
+def send_email():
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
 
-# Create the email message
-msg = MIMEMultipart()
-msg['From'] = sender_email
-msg['To'] = recipient_email
-msg['Subject'] = subject
-msg.attach(MIMEText(message, 'plain'))
+    f = open("user.json")
+    data = json.load(f)
+    recipient_email = data['email']
+    print("send email to ", recipient_email)
+    f.close()
 
-# Establish a secure SMTP connection
-try:
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
+    f = open("email-config.json")
+    data = json.load(f)
+    sender_email = data['sender_email']
+    smtp_username = sender_email
+    smtp_password = data['sender_pwd']
+    f.close()
 
-    # Send the email
-    server.sendmail(sender_email, recipient_email, msg.as_string())
+    # Email configuration
+    subject = "Get free coffee from NIDA Hub Smartbin"
+    message = "This is your coupon from smartbin. You got 1 free cup of coffee"
 
-    print("Email sent successfully!")
-except Exception as e:
-    print("Error: Unable to send email.")
-    print(e)
-finally:
-    # Close the SMTP server connection
-    server.quit()
+    # SMTP server configuration for Outlook
+    smtp_server = "smtp.office365.com"
+    smtp_port = 587  # For TLS
+
+    # Create the email message
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Attach coupou image
+    from email.mime.image import MIMEImage
+    ImgFileName = "img_coupon.jpg"
+    with open(ImgFileName, 'rb') as f:
+        img_data = f.read()
+    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+    msg.attach(image)
+
+    # Establish a secure SMTP connection
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+
+        # Send the email
+        server.sendmail(sender_email, recipient_email, msg.as_string())
+
+        print("Email sent successfully!")
+    except Exception as e:
+        print("Error: Unable to send email.")
+        print(e)
+    finally:
+        # Close the SMTP server connection
+        server.quit()
+
+
+
+if __name__ == "__main__":
+    send_email()
